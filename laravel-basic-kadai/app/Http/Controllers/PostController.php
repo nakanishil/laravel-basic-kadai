7cd3aed3a679028e0c1159dfb1cfe1de1e3d07d8
nakanishil <nakanishi.jsdf@gmail.com>
2025-07-01 16:21:20 +0900

    データの作成機能とバリデーションを実装しようの課題分

--- laravel-basic-kadai/app/Http/Controllers/PostController.php
+++ laravel-basic-kadai/app/Http/Controllers/PostController.php
@@ -19,6 +19,27 @@ class PostController extends Controller
         // URL /posts/{id}のidと主キーが一致するデータをpostsテーブルから取得
         $post = Post::find($id);
 
-        return view('posts/show', compact('post'));
+        return view('posts.show', compact('post'));
+    }
+
+    public function create() {
+        return view('posts.create');
+    }
+
+    public function store(Request $request) {
+        // バリデーション設定
+        $request->validate([
+            'title' => 'required|max:20',
+            'content' => 'required|max:200'
+        ]);
+
+        // フォーム入力内容を基にテーブルにデータを追加
+        $post = new Post();
+        $post->title = $request->input('title');
+        $post->content = $request->input('content');
+        $post->save();
+
+        // リダイレクト
+        return redirect("/posts");
     }
 }
--- laravel-basic-kadai/routes/web.php
+++ laravel-basic-kadai/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
+use Illuminate\Foundation\Events\VendorTagPublished;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,4 +25,9 @@ Route::get('posts', function() {
 
 Route::get('/posts', [PostController::class, 'index']);
 
+Route::get('/posts/create', [PostController::class, 'create']);
+
+Route::Post('/posts/store', [PostController::class, 'store']) ->name('posts.store');
+
 Route::get('/posts/{id}', [PostController::class, 'show']);
+
