73f61d92d1ae96516f01a6255f4c671fc2a12ebb
nakanishil <nakanishi.jsdf@gmail.com>
2025-07-03 15:00:20 +0900

    auth kadai

--- laravel-basic-kadai/routes/web buckup.php
+++ laravel-basic-kadai/routes/web buckup.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use Illuminate\Foundation\Events\VendorTagPublished;
@@ -20,30 +19,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::get('posts', function() {
     return view('posts/index');
 });
 
 Route::get('/posts', [PostController::class, 'index']);
 
-Route::get('/posts/create', [PostController::class, 'create'])->middleware('auth');
+Route::get('/posts/create', [PostController::class, 'create']);
 
-Route::Post('/posts/store', [PostController::class, 'store']) ->name('posts.store')->middleware('auth');
+Route::Post('/posts/store', [PostController::class, 'store']) ->name('posts.store');
 
 Route::get('/posts/{id}', [PostController::class, 'show']);
 
-require __DIR__.'/auth.php';
